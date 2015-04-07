cd874dfccbafc886796cc356586dde2796adf1e2
Jon Cuthbert <jon@cuth.net>
2015-04-07 12:35:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+bullet-links
+============
+
+This module builds a list of bullet links. If the index changes outside of this module, it is important to notify this module of the new index.
+
+ES5 polyfills needed for `Array.forEach`.
+
+Dependencies: jQuery
+
+Basic example
+-------------
+
+```js
+// How many bullet links do you want?
+var count = 6;
+
+// This wouldn't be necessary since all these options are the defaults
+var options = {
+    template: '<a href="#" {attribute}={status}><span class="Hidden">Go to item {index}</span></a>',
+    target: '',
+    attribute: 'data-status',
+    active: 'active',
+    inactive: 'inactive',
+    initial: 0
+};
+
+var bullets = new BulletLinks('#el', count, options);
+
+bullets.onUpdate(function (index) {
+    console.log(index); 
+});
+```js
+
+
+BulletLinks
+------------
+Constructor  
+@param {jQuery} - element that the bullet links go inside  
+@param {Number} - the amount of items you want to generate  
+@param {Object} - object to change any of the default options  
+@return {Object} - instance of BulletLinks  
+
+
+.setIndex
+---------
+@param {Number}  
+@return {Object} - instance of BulletLinks  
+
+
+.onUpdate
+---------
+@param {Function}  
+@return {Object} - instance of BulletLinks  
+
+
+.appendTo
+-------
+@param {jQuery} - element you want to append all the bullets and container to
+@return {Object} - instance of BulletLinks  
---
+++ bullet-links.js
@@ -0,0 +1,105 @@
+/*  bullet-links
+ *  version: 1.0.0
+ *  https://stash.c2mpg.com:8443/projects/C2/repos/bullet-links
+ *  @preserve
+ */
+
+/* exported Bullets */
+
+var BulletLinks = (function ($) {
+    'use strict';
+
+    var defaults = {
+        template: '<a href="#" {attribute}={status}><span class="Hidden">Go to item {index}</span></a>',
+        target: '',
+        attribute: 'data-status',
+        active: 'active',
+        inactive: 'inactive',
+        initial: 0
+    };
+
+    // context should be an instance of Bullets
+    var generateMarkup = function (index) {
+        var values = {
+            attribute: this.opts.attribute,
+            status: (index === this.opts.initial) ? this.opts.active : this.opts.inactive,
+            index: index + 1
+        };
+        return this.opts.template.replace(/{([a-z]+)}/g, function (match, key) {
+            return (typeof values[key] === 'undefined') ? match : values[key];
+        });
+    };
+
+    // context should be an instance of Bullets
+    var createHTML = function () {
+        var items = [];
+        for (var x = 0; x < this.count; x += 1) {
+            items.push($(generateMarkup.call(this, x))[0]);
+        }
+        return $(items);
+    };
+
+    // context should be an instance of Bullets
+    var emit = function (index) {
+        this._listeners.forEach(function (cb) {
+            cb(index);
+        });
+    };
+
+    // context should be an instance of Bullets
+    var activate = function (index) {
+        if (index === this.index) return;
+        this.$items.eq(this.index).attr(this.opts.attribute, this.opts.inactive);
+        this.$items.eq(index).attr(this.opts.attribute, this.opts.active);
+        this.index = index;
+        return this;
+    };
+
+    var bindEvents = function () {
+        var self = this;
+        if (this.opts.target) {
+            this.$items.on('click', this.opts.target, function (e) {
+                var index = self.$items.index(e.delegateTarget);
+                activate.call(self, index);
+                emit.call(self, index);
+                e.preventDefault();
+            });
+        } else {
+            this.$items.on('click', function (e) {
+                var index = self.$items.index(e.currentTarget);
+                activate.call(self, index);
+                emit.call(self, index);
+                e.preventDefault();
+            });
+        }
+    };
+
+    var Bullets = function (el, count, options) {
+        this.$el = $(el);
+        this.count = count;
+        this.opts = $.extend({}, defaults, options);
+        this._listeners = [];
+
+        this.$items = createHTML.call(this);
+        this.$el.append(this.$items);
+
+        this.index = this.opts.initial;
+
+        bindEvents.call(this);
+    };
+
+    Bullets.prototype.setIndex = activate;
+
+    Bullets.prototype.onUpdate = function (cb) {
+        if (typeof cb !== 'function') return;
+        this._listeners.push(cb);
+        return this;
+    };
+
+    Bullets.prototype.appendTo = function (selector) {
+        this.$parent = $(selector).append(this.$el);
+        return this;
+    };
+
+    return Bullets;
+}(jQuery));
